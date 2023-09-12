3684485f90b204ee28d8011a337e7856dc79ea84
ruoyu <709421127@qq.com>
2023-09-12 21:21:06 +0930

    results

--- run_test.py
+++ run_test.py
@@ -4,6 +4,7 @@ import io
 import sys
 import memsim  # 导入主程序模块
 import matplotlib.pyplot as plt
+import os
 
 class TestMainFunction(unittest.TestCase):
     def setUp(self):
@@ -35,7 +36,19 @@ class TestMainFunction(unittest.TestCase):
                     with patch("sys.argv", args):
                         # 调用主函数
                         output.append(memsim.main())
-                line1_handle, =ax.plot(output,  linestyle='-',label=m)
+                #创建文件夹output/traces[i]，如果已经存在则不创建
+                os.makedirs('./output/'+traces[i], exist_ok=True)
+                
+                               
+                #output写入名为m的文件 路径为./output/traces[i]/m.txt，m是mmu的名称。output每个元素占一行，前面加上行号+1
+               
+
+                f = open('./output/'+traces[i]+'/'+m+'.txt','w')
+                for j in range(len(output)):
+                    f.write(str(j+1)+' '+str(output[j])+'\n')
+                ##以output绘制折线图，x轴为1-output的长度，y轴为output的值
+               
+                line1_handle, = ax.plot(range(1,len(output)+1),output,  linestyle='-',label=m)
                 line_handle.append(line1_handle)
                 
                 #显示最接近0.05的值
@@ -44,9 +57,15 @@ class TestMainFunction(unittest.TestCase):
                     if abs(output[j]-0.05) < min:
                         min = abs(output[j]-0.05)
                         index = j
-                ax.plot(index,output[index], 'ro')
+                index+=1
+                ax.plot(index,output[index-1], 'ro')
                 #显示最接近0.05的坐标
-                ax.annotate('(%d,%.3f)'%(index,output[index]), xy=(index, output[index]), xytext=(index, output[index]+0.01))
+                ax.annotate('(%d,%.3f)'%(index,output[index-1]), xy=(index, output[index-1]), xytext=(index, output[index-1]+0.01))
+                # #显示x为60和120时的值
+                # ax.plot(60,output[60], 'ro')
+                # ax.annotate('(%d,%.3f)'%(60,output[60]), xy=(60, output[60]), xytext=(60, output[60]+0.01))
+                # ax.plot(120,output[120], 'ro')
+                # ax.annotate('(%d,%.3f)'%(120,output[120]), xy=(120, output[120]), xytext=(120, output[120]+0.01))
                 
 
 
